ed3e8172c4a75fcbca8a3f22e30d7ac75717ad4e
Simon Glez <115299562+Cglezf@users.noreply.github.com>
2025-05-25 11:10:55 -0600

    chore: set up project with structure, arithmetic ops and formatter

--- src/SimpleAlgebra.jl
+++ src/SimpleAlgebra.jl
@@ -1,5 +1,27 @@
 module SimpleAlgebra
+export add, sub, mul, div_safe
 
-# Write your package code here.
-
+function add(a::T, b::T)::T where {T<:Real}
+    @info "Adding $a and $b"
+    return a + b
 end
+
+function sub(a::T, b::T)::T where {T<:Real}
+    @info "Subtracting $b from $a"
+    return a - b
+end
+
+function mul(a::T, b::T)::T where {T<:Real}
+    @info "Multiplying $a and $b"
+    return a * b
+end
+
+function div_safe(a::T, b::T)::T where {T<:Real}
+    @info "Dividing $a by $b"
+    if iszero(b)
+        throw(DivideError())
+    end
+    return a / b
+end
+
+end # modulo
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using SimpleAlgebra
 using Test
 
-@testset "SimpleAlgebra.jl" begin
-    # Write your tests here.
+@testset "SimpleAlgebra sums" begin
+    @test add(2, 3) == 5
+    @test add(0, 0) == 0
+    @test add(-1, 1) == 0
+    @test add(-1, -1) == -2
+    @test add(0.5, 0.5) == 1.0
+    @test add(1//2, 2//3) == 7//6
+end
+
+@testset "SimpleAlgebra types" begin
+    @test isapprox(add(0.1, 0.2), 0.3; atol=1e-10)
+    @test_throws MethodError add(1 + 2im, 3 + 4im)
+    @test_throws MethodError add(1, 0 + 0im, 2.0 - 1im)
 end
