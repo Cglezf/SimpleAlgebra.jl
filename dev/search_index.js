3731824f191c07666d80d5e43aa861d182d7be01
Documenter.jl <documenter@juliadocs.github.io>
2025-05-25 17:46:55 +0000

    build based on ed3e817

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SimpleAlgebra","page":"Home","title":"SimpleAlgebra","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimpleAlgebra.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
